ea9fc44a0c5444ff1d541460bdb3b9dd78b64be3
ericpham <phamwangthang@gmail.com>
2025-07-04 21:34:01 +0200

    test without carousel

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,7 +3,7 @@
     :style="{ backgroundImage: `url(${herobg})` }" style="touch-action: manipulation;">
     <div class="absolute inset-0 bg-[#2F1893] opacity-50 z-0"></div>
     <div class="relative z-10 flex flex-col items-center justify-center text-center h-full overflow-hidden">
-        <HeroCarousel />
+        <!-- <HeroCarousel /> -->
       <!-- cta button -->
       <BaseButton href="#cta" class="bg-[#482BE7] hover:bg-blue-700 h-[60px] px-9 text-[18px] mt-8"
         text="Create an Account" />
@@ -13,6 +13,6 @@
 
 <script setup>
 import herobg from "@/assets/images/hero.jpg";
-import HeroCarousel from "./HeroCarousel.vue";
+// import HeroCarousel from "./HeroCarousel.vue";
 import BaseButton from "./BaseButton.vue";
 </script>
